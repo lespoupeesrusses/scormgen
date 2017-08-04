0fd6a973426bde8f1bd5fa2153f4a9a222c6c7a5
Arnaud Levy <arnaud@semiodesign.com>
2017-08-04 17:23:25 +0200

    generator

--- lib/scormgen/generator.rb
+++ lib/scormgen/generator.rb
@@ -1,23 +1,23 @@
 module Scormgen
   class Generator
-
     def run
       puts "Generate"
+      list_files
+      puts "#{@files.count} files found"
     end
 
     protected
 
     def list_files
-      files = Dir.glob("**/*")
-      files.each do |path|
+      @files = []
+      Dir.glob("**/*").each do |path|
         next if File.directory? path
         next if ignored_files.include? path
-        imsmanifest += "      <file href=\"#{ path }\" />
-      "
+        @files << path
       end
     end
 
-    def ignore
+    def ignored_files
       ['scorm_generator.rb', 'fire_app_log.txt', 'crossdomain.xml', 'config.rb']
     end
   end
